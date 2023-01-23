f7f10077df00c871cc75385474872d5bc5910498
Jeffrey Monzili <jeffrey.monzili@gmail.com>
2023-01-23 18:04:50 +0100

    delete post in the DB.

--- back/controllers/posts.js
+++ back/controllers/posts.js
@@ -1,46 +1,5 @@
 const { prisma } = require('../db/db');
 
-const comment1 = {
-  user: 'test2@gmmail.com',
-  content: 'This is my comment',
-  id: '1',
-};
-
-const comment2 = {
-  user: 'test3@gmmail.com',
-  content: 'This is my comment',
-  id: '2',
-};
-
-const post1 = {
-  user: 'test1@gmail.com',
-  title: 'my first post',
-  content: 'This is my first post',
-  imageUrl: 'https://picsum.photos/400/300',
-  comments: [comment2, comment1],
-  id: '1',
-};
-
-const post2 = {
-  user: 'test2@gmail.com',
-  title: 'my second post',
-  content: 'This is my second post',
-  imageUrl: 'https://picsum.photos/400/300',
-  comments: [],
-  id: '2',
-};
-
-const post3 = {
-  user: 'test3@gmail.com',
-  title: 'my third post',
-  content: 'This is my third post',
-  imageUrl: 'https://picsum.photos/400/300',
-  comments: [comment1],
-  id: '3',
-};
-
-const posts = [post1, post2, post3];
-
 async function getPosts(req, res) {
   const email = req.email;
   const posts = await prisma.post.findMany({
@@ -98,6 +57,39 @@ function addImageUrl(req, post) {
   post.imageUrl = url;
 }
 
+async function deletePost(req, res) {
+  const postId = Number(req.params.id);
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: {
+        user: {
+          select: {
+            email: true,
+          },
+        },
+      },
+    });
+    console.log('post to delete:', post);
+    if (post == null) {
+      return res.status(404).send({ error: 'Post not found' });
+    }
+    const email = req.email;
+    if (email !== post.user.email) {
+      return res
+        .status(404)
+        .send({ error: 'You are not the owner of this post' });
+    }
+    //  Suppression des commentaire
+    await prisma.comment.deleteMany({ where: { postId } });
+    //  Puis suppression du post
+    await prisma.post.delete({ where: { id: postId } });
+    res.send({ message: 'Post deleted' });
+  } catch (err) {
+    res.status(500).send({ error: 'Something went wrong' });
+  }
+}
+
 async function createComment(req, res) {
   const postId = Number(req.params.id);
   // const post = posts.find((post) => post.id === postId);
@@ -123,43 +115,4 @@ async function createComment(req, res) {
   res.send({ comment });
 }
 
-function deletePost(req, res) {
-  const postId = req.params.id;
-  const post = posts.find((post) => post.id === postId);
-  if (post == null) {
-    return res.status(404).send({ error: 'Post not found' });
-  }
-  const index = posts.indexOf(post);
-  posts.splice(index, 1);
-  deleteComments(post);
-  res.send({ message: `Post ${postId} was deleted`, posts });
-}
-
-function deleteComments(post) {
-  post.comments = [];
-}
-
 module.exports = { getPosts, createPosts, deletePost, createComment };
-
-// async function createComment(req, res) {
-//   const postId = Number(req.params.id);
-//   const post = await prisma.post.findUnique({
-//     where: { id: postId },
-//     include: {
-//       user: {
-//         select: {
-//           id: true,
-//         },
-//       },
-//     },
-//   });
-//   console.log('post commenter:', post);
-//   if (post == null) {
-//     return res.status(404).send({ error: 'Post not found !' });
-//   }
-
-//   const userId = req.email;
-//   // const commentToSend = { userId, postId, content: req.body.comment };
-//   // post.comments.push(commentToSend);
-//   // res.send({ post });
-// }
